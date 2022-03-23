2648258f72a467e891ede4cf095c78d27e079c8f
akashpk41 <akashpk84100@gmail.com>
2022-03-23 21:13:51 +0600

    data loaded from mealdb api, successfully

--- src/components/Foods/Food/Food.jsx
+++ src/components/Foods/Food/Food.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Food = () => {
+const Food = ({food}) => {
+     console.log(food);
+
      return (
-          <div>
-               
+          <div  >
+
           </div>
      );
 };
--- src/components/Foods/Foods.jsx
+++ src/components/Foods/Foods.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
+import Food from "./Food/Food";
 
 const Foods = () => {
-     const [foods,setFoods] = useState([])
-     // ! fetch all food data from mealdb api
-     useEffect( () => {
-          fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-          .then(res => res.json())
-          .then(data => console.log(data)
-          )
-     } ,[])
+  const [foods, setFoods] = useState([]);
+  // ! fetch all food data from mealdb api
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+      .then((res) => res.json())
+      .then((data) => setFoods(data.meals));
+  }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>total foods {foods.length}</h1>
+      {foods.map((food) => (
+        <Food key={food.idMeal} food={food} />
+      ))}
+    </div>
+  );
 };
 
 export default Foods;
